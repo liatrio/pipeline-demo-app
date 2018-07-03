19b71fd825bcf0ae011874d8041feabb2cb81299
Steven Oetting <steveno@liatrio.com>
2018-07-03 10:03:27 -0700

    commented out workspace select

--- destroy.sh
+++ destroy.sh
@@ -12,7 +12,7 @@ terraform --version
 export TF_WORKSPACE=$TF_VAR_instance_name
 
 #create and switch to a new workspace
-terraform workspace select $TF_VAR_instance_name
+#terraform workspace select $TF_VAR_instance_name
 
 # destroy the instance
 terraform destroy -input=false -auto-approve=true
