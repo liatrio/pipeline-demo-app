63f9dfa88c8575600ce615bf5f98abd29ad20c69
Steven Oetting <steveno@liatrio.com>
2018-07-03 10:38:52 -0700

    added no-color flag to get rid of special characters in logs

--- destroy.sh
+++ destroy.sh
@@ -3,12 +3,12 @@ workspace=$(dirname 0)
 
 # initialize the workspace
 #this is necessary for using remote state storage
-terraform init -input=false
+terraform init -input=false -no-color
 
 #create workspace
 export TF_WORKSPACE=$TF_VAR_instance_name
 
 # destroy the instance
-terraform destroy -force
+terraform destroy -force -no-color
 
 exit $EXIT_STATUS
--- terraform.sh
+++ terraform.sh
@@ -3,18 +3,18 @@ workspace=$(dirname 0)
 
 # initialize the workspace
 #this is necessary for using remote state storage
-terraform init -input=false
+terraform init -input=false -no-color
 
 #create workspace
 export TF_WORKSPACE=$TF_VAR_instance_name
 
 #create and switch to a new workspace
-terraform workspace new $TF_VAR_instance_name
+terraform workspace new $TF_VAR_instance_name -no-color
 
 #create the tfplan
-terraform plan -out=tfplan -input=false
+terraform plan -out=tfplan -input=false -no-color
 
 # initialize the instance
-terraform apply -input=false tfplan
+terraform apply -input=false tfplan -no-color
 
 exit $EXIT_STATUS
